190f5bf4857f9723bd98ec3c6ac6d91fa9d8b66d
Domingo Suarez Torres <domingo.suarez@gmail.com>
2011-08-15 01:34:19 -0500

    updated to Grails SpringSocial Core plugin 0.1.4

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -39,7 +39,7 @@ grails.project.dependency.resolution = {
         //mavenRepo "http://repository.codehaus.org"
         //mavenRepo "http://download.java.net/maven/2/"
         //mavenRepo "http://repository.jboss.com/maven2/"
-		mavenRepo "http://maven.springframework.org/release"
+        mavenRepo "http://maven.springframework.org/release"
         mavenRepo "http://maven.springframework.org/snapshot"
         mavenRepo "http://maven.springframework.org/milestone"
     }
@@ -48,6 +48,6 @@ grails.project.dependency.resolution = {
         compile("org.springframework.social:spring-social-twitter:${springSocialVersion}") { transitive = false }
     }
     plugins {
-        runtime (":spring-social-core:0.1.2")
+        compile(":spring-social-core:0.1.4")
     }
 }
