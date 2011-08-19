faf431c35f1c57c287e74e0ed9ffc2c3572cdcd8
Domingo Suarez Torres <domingo.suarez@gmail.com>
2011-08-19 12:27:16 -0500

    setting version to 0.1.5, adding depends on SpringSocial Core plugin

--- SpringSocialTwitterGrailsPlugin.groovy
+++ SpringSocialTwitterGrailsPlugin.groovy
@@ -1,10 +1,10 @@
 class SpringSocialTwitterGrailsPlugin {
     // the plugin version
-    def version = "0.1.4"
+    def version = "0.1.5"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.3 > *"
     // the other plugins this plugin depends on
-    def dependsOn = [:]
+    def dependsOn = [springSocialCore: '0.1 > *']
     // resources that are excluded from plugin packaging
     def pluginExcludes = [
             "grails-app/views/error.gsp"
