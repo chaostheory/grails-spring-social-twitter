6715e4da7c5e4504cb6871a907f3188b093f92da
Domingo Suarez Torres <domingo.suarez@gmail.com>
2011-10-04 22:35:27 -0500

    prepare 0.1.15 release

--- SpringSocialTwitterGrailsPlugin.groovy
+++ SpringSocialTwitterGrailsPlugin.groovy
@@ -1,25 +1,22 @@
 class SpringSocialTwitterGrailsPlugin {
   // the plugin version
-  def version = "0.1.14"
+  String version = "0.1.15"
   // the version or versions of Grails the plugin is designed for
-  def grailsVersion = "1.3.0 > *"
+  String grailsVersion = "1.3.0 > *"
   // the other plugins this plugin depends on
-  Map dependsOn = ['springSocialCore': '0.1.14 > *']
+  Map dependsOn = ['springSocialCore': '0.1.15 > *']
   // resources that are excluded from plugin packaging
-  def pluginExcludes = [
+  List pluginExcludes = [
       "grails-app/views/error.gsp"
   ]
 
-  def author = "Domingo Suarez Torres"
-  def authorEmail = "domingo.suarez@gmail.com"
-  def title = "Spring Social Twitter"
-  def description = '''\\
-Spring Social Twitter plugin.
-'''
-
+  String author = "Domingo Suarez Torres"
+  String authorEmail = "domingo.suarez@gmail.com"
+  String title = "Spring Social Twitter"
+  String description = 'Spring Social Twitter plugin.'
 
   // URL to the plugin's documentation
-  def documentation = "http://grails.org/plugin/spring-social-twitter"
+  String documentation = "http://grails.org/plugin/spring-social-twitter"
 
   def doWithWebDescriptor = { xml ->
     // TODO Implement additions to web.xml (optional), this event occurs before
