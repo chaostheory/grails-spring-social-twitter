ff11e4f03b473f9587bf536ed23d17a469fd4920
Domingo Suarez Torres <domingo.suarez@gmail.com>
2011-10-04 23:48:12 -0500

    prepare 0.1.17 release

--- SpringSocialTwitterGrailsPlugin.groovy
+++ SpringSocialTwitterGrailsPlugin.groovy
@@ -1,10 +1,10 @@
 class SpringSocialTwitterGrailsPlugin {
   // the plugin version
-  String version = "0.1.16"
+  String version = "0.1.17"
   // the version or versions of Grails the plugin is designed for
   String grailsVersion = "1.3.0 > *"
   // the other plugins this plugin depends on
-  Map dependsOn = ['springSocialCore': '0.1.16 > *']
+  Map dependsOn = ['springSocialCore': '0.1.17 > *']
   // resources that are excluded from plugin packaging
   List pluginExcludes = [
       "grails-app/views/error.gsp"
