a961c3fb8be9cc5cd37e6819602dfe950e3ffc63
Domingo Suarez Torres <domingo.suarez@gmail.com>
2011-10-04 19:49:48 -0500

    preparing the 0.1.14 release

--- SpringSocialTwitterGrailsPlugin.groovy
+++ SpringSocialTwitterGrailsPlugin.groovy
@@ -1,10 +1,10 @@
 class SpringSocialTwitterGrailsPlugin {
   // the plugin version
-  def version = "0.1.13"
+  def version = "0.1.14"
   // the version or versions of Grails the plugin is designed for
   def grailsVersion = "1.3.0 > *"
   // the other plugins this plugin depends on
-  Map dependsOn = ['springSocialCore': '0.1.13 > *']
+  Map dependsOn = ['springSocialCore': '0.1.14 > *']
   // resources that are excluded from plugin packaging
   def pluginExcludes = [
       "grails-app/views/error.gsp"
@@ -31,22 +31,4 @@ Spring Social Twitter plugin.
     context.'component-scan'('base-package': "grails.plugins.springsocial.config.twitter")
   }
 
-  def doWithDynamicMethods = { ctx ->
-    // TODO Implement registering dynamic methods to classes (optional)
-  }
-
-  def doWithApplicationContext = { applicationContext ->
-    // TODO Implement post initialization spring config (optional)
-  }
-
-  def onChange = { event ->
-    // TODO Implement code that is executed when any artefact that this plugin is
-    // watching is modified and reloaded. The event contains: event.source,
-    // event.application, event.manager, event.ctx, and event.plugin.
-  }
-
-  def onConfigChange = { event ->
-    // TODO Implement code that is executed when the project configuration changes.
-    // The event is the same as for 'onChange'.
-  }
 }
