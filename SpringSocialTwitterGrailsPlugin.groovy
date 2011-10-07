17fb679198e0e77922768de1551559c8621c1746
Domingo Suarez Torres <domingo.suarez@gmail.com>
2011-10-07 17:27:31 -0500

    prepare 0.1.28 release

--- SpringSocialTwitterGrailsPlugin.groovy
+++ SpringSocialTwitterGrailsPlugin.groovy
@@ -1,10 +1,10 @@
 class SpringSocialTwitterGrailsPlugin {
   // the plugin version
-  String version = "0.1.27"
+  String version = "0.1.28"
   // the version or versions of Grails the plugin is designed for
   String grailsVersion = "1.3.0 > *"
   // the other plugins this plugin depends on
-  Map dependsOn = ['springSocialCore': '0.1.27 > *']
+  Map dependsOn = ['springSocialCore': '0.1.28 > *']
   // resources that are excluded from plugin packaging
   List pluginExcludes = [
       "grails-app/views/error.gsp"
@@ -15,15 +15,9 @@ class SpringSocialTwitterGrailsPlugin {
   String title = "Spring Social Twitter"
   String description = 'Spring Social Twitter plugin.'
 
-  // URL to the plugin's documentation
   String documentation = "http://grails.org/plugin/spring-social-twitter"
 
-  def doWithWebDescriptor = { xml ->
-    // TODO Implement additions to web.xml (optional), this event occurs before
-  }
-
   def doWithSpring = {
-    // TODO Implement runtime spring config (optional)
     xmlns context: "http://www.springframework.org/schema/context"
     context.'component-scan'('base-package': "grails.plugins.springsocial.config.twitter")
   }
